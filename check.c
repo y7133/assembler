44bf3aa9b9911a6b6e71374ec6f695159f555e23
y7133 <y7133@naver.com>
2018-11-12 21:31:21 +0900

    first commit(make check.c,trans.c)

--- check.c
+++ check.c
@@ -1,10 +1,37 @@
 #include "myas.h"
-
+#include<string.h>
 int is_valid(char *op, char *args)
 {
 	//printf("if valid, return 1\n");
 	//printf("otherwise, return 0\n");
-
+	char x[30],y[30];
+	char *r,*s,*p,*q;
+	int i,j;
+	for(i=0;i<strlen(args);i++)
+	{
+		if(args[i]==',')
+		 break;
+		x[i]=args[i];
+	}
+	i++;
+	for(j=i;j<strlen(args)-i;j++)
+	{
+		y[j-i]=args[j];
+	}
+	r=strstr(y,"0x");
+	if(r!=NULL)
+	 s=strstr(r,"(%");
+	p=strstr(x,"0x");
+	if(p!=NULL)
+	 q=strstr(x,"(%");
+	if(x[0]=='('&&(y[0]=='('||r!=NULL||s!=NULL))
+		return 0;
+	else if(q!=NULL&&(y[0]=='('||r!=NULL||s!=NULL))
+		return 0;
+	else if(x[0]=='0'&&x[1]=='x'&&(y[0]=='('||r!=NULL||s!=NULL))
+		return 0;
+	else if(x[0]=='$'&&y[0]=='$')
+		return 0;
 	/********************************/
     /*  							*/
 	/*         syntax check         */
--- trans.c
+++ trans.c
@@ -1,8 +1,12 @@
 #include "myas.h"
+#include<string.h>
 
 int instr_trans(char *op, char *args, char* mcode)
 {
-	// check syntax 
+	// check syntax
+        char x[30],y[30],z[30];
+	char *s;
+	int i,n,j;
 	if(!is_valid(op, args)){
 		printf("Error: %s %s is not valid\n", op, args);
 		return 0;
@@ -10,7 +14,61 @@ int instr_trans(char *op, char *args, char* mcode)
 
 
 	strcpy(mcode, "AB CD EF");
-
+        for( i=0;i<strlen(args);i++)
+ 	{
+ 	    if(args[i]==',')
+		break;
+  	    x[i]=args[i];
+ 	}
+	++i;
+        for(j=i;j<strlen(args)-i;j++)
+	{
+		y[j-i]=args[j];
+	}
+	char *r=strstr(x,"0x");
+	if(r!=NULL)
+		s=strstr(r,"(%");	
+        if(x[0]=='%'&&y[0]=='%')
+         {
+		strcpy(mcode, "1000 10001");
+	 }
+	else if((x[0]=='('&&y[0]=='%')||(x[0]=='%'&&y[0]=='('))
+	{
+	 	strcpy(mcode, "1000 1011");
+	}
+	else if(s!=NULL&&y[0]=='%')
+	{
+		strcpy(mcode, "1000 1011");
+	}
+	else if(x[0]=='0'&&x[1]=='x'&&strcmp(y,"%eax")==0)
+	{
+		strcpy(mcode, "1010 0001");
+	}
+	else if(strcmp(x,"%eax")==0&&y[1]=='x'&&y[0]=='0')
+	{
+		strcpy(mcode, "1010 0011");
+	}
+	else if(x[0]=='$'&&y[0]=='%')
+	{
+		if(strcmp(y,"%eax")==0)
+		  strcpy(mcode, "1011 1000");
+		else if(strcmp(y,"%ecx")==0)
+		  strcpy(mcode, "1011 1001");
+		else if(strcmp(y,"%edx")==0)
+		 strcpy(mcode, "1011 1010");
+		else if(strcmp(y,"%ebx")==0)
+		 strcpy(mcode, "1011 1011");
+		else if(strcmp(y,"%esp")==0)
+		 strcpy(mcode, "1011 1100");
+		else if(strcmp(y,"%ebp")==0)
+		 strcpy(mcode, "1011 1101");
+		else if(strcmp(y,"%esi")==0)
+		 strcpy(mcode, "1011 1110");
+		else if(strcmp(y,"%edi")==0)
+		 strcpy(mcode, "1011 1111");
+		else
+		 strcpy(mcode,"error");
+	}
 	/********************************/
     /*  							*/
 	/*    generate machine code     */
