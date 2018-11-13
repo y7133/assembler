3def135e0e610ea600d121ce7fa7d064c1510457
y7133 <y7133@naver.com>
2018-11-13 10:05:33 +0900

    fourth commit (fix error)

--- trans.c
+++ trans.c
@@ -17,60 +17,63 @@ int instr_trans(char *op, char *args, char* mcode)
   	    x[i]=args[i];
  	}
 	++i;
-	printf("%c  ",args[i]);
+	//printf("%c  ",args[i]);
         for(j=i;j<strlen(args);j++)
 	{
 		y[j-i]=args[j];
 	}
-	printf("%c  ",y[j]);
-	printf("x:%s i:%d  y:%s\n",x,i,y);
+	//printf("%c  ",y[j]);
+	//printf("x:%s i:%d  y:%s\n",x,i,y);
 	if(!is_valid(op,args)){
 		printf("Error:%s %s is not valid\n",op,args);
 		return 0;
 	}
-	//char *r=strstr(x,"0x");
-	//if(r!=NULL)
-	//	s=strstr(r,"(%");	
+	char *r=strstr(x,"0x");
+	if(r!=NULL)
+		s=strstr(r,"(%");	
         if(x[0]=='%'&&y[0]=='%')
          {
-		strcpy(mcode, "1000 10001");
+		strcpy(mcode, "89");
 	 }
 	else if((x[0]=='('&&y[0]=='%')||(x[0]=='%'&&y[0]=='('))
 	{
-	 	strcpy(mcode, "1000 1011");
+	 	strcpy(mcode, "8b");
 	}
-	else if(x[0]=='0'&&x[1]=='x'&&y[0]=='%')
+	else if(r!=NULL&&x[0]=='0'&&y[0]=='%')
 	{
-		strcpy(mcode, "1000 1011");
+		strcpy(mcode, "8b");
+	}
+	else if(s!=NULL&&y[0]=='%')
+	{
+		strcpy(mcode,"8b");
 	}
 	else if(x[0]=='0'&&x[1]=='x'&&strcmp(y,"%eax")==0)
 	{
-		strcpy(mcode, "1010 0001");
+		strcpy(mcode, "a1");
 	}
 	else if(strcmp(x,"%eax")==0&&y[1]=='x'&&y[0]=='0')
 	{
-		strcpy(mcode, "1010 0011");
+		strcpy(mcode, "a3");
 	}
 	else if(x[0]=='$'&&y[0]=='%')
 	{
 		if(strcmp(y,"%eax")==0)
-		  strcpy(mcode, "1011 1000");
+		  strcpy(mcode, "b8");
 		else if(strcmp(y,"%ecx")==0)
-		  strcpy(mcode, "1011 1001");
+		  strcpy(mcode, "b9");
 		else if(strcmp(y,"%edx")==0)
-		 strcpy(mcode, "1011 1010");
+		 strcpy(mcode, "ba");
 		else if(strcmp(y,"%ebx")==0)
-		 strcpy(mcode, "1011 1011");
+		 strcpy(mcode, "bb");
 		else if(strcmp(y,"%esp")==0)
-		 strcpy(mcode, "1011 1100");
+		 strcpy(mcode, "bc");
 		else if(strcmp(y,"%ebp")==0)
-		 strcpy(mcode, "1011 1101");
+		 strcpy(mcode, "bd");
 		else if(strcmp(y,"%esi")==0)
-		 strcpy(mcode, "1011 1110");
+		 strcpy(mcode, "be");
 		else if(strcmp(y,"%edi")==0)
-		 strcpy(mcode, "1011 1111");
-		else
-		 strcpy(mcode,"error");
+		 strcpy(mcode, "bf");
+		
 	}
 	/********************************/
     /*  							*/
